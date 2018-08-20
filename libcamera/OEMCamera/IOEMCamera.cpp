7004a79370fa4a361d3feb676ce916d2c3187126
Sungho <skyreon@gmail.com>
2018-08-20 13:47:21 +0900

    Update IOEMCamera.cpp

--- libcamera/OEMCamera/IOEMCamera.cpp
+++ libcamera/OEMCamera/IOEMCamera.cpp
@@ -2,23 +2,35 @@ define LOG_NDEBUG 0
 #define LOG_TAG "IOEMCamera"
 
 #include "IOEMCamera.h"
-
+#define FLANE_MAX 1
+    
 IOEMCamera::~IOEMCamera()
 {
 }
 
-bool IOEMCamera::nativePreviewThread()
+bool IOEMCamera::IOEMPreviewThread()
 {
-    srcAddr = mFliteNode.buffer[index].virt.extP[plane];
-    dstAddr = dstBuf.virt.extP[plane];
-    if(srcAddr == NULL ||  dstAddr == NULL) {
-        ALOGI(" **** previewZoomThread: Invalid memory");
-    } else {
-        memcpy(dstAddr, srcAddr, dstBuf.size.extS[plane]);
-        mDataCb(CAMERA_MSG_PREVIEW_FRAME, mPreviewHeap, index, NULL, mCallback);
-        /* Display preview in HAL */
-        nativeFlushPreview(dstBuf.virt.extP[plane]);
-    }
+    char *srcAddr, dstAddr;
+    int index = getPreviewIndex();
     
+    dstBuf.virt.p = (char *)mCamHeap->data + (index * (mCamSize-SPARE_SIZE * SPARE_PLANE));
+    dstBuf.fd.extFd[0] = mCamFd;
+    
+    for (int plane = 0; plane < FLANE_MAX; plane++) {
+        srcAddr = mFliteNode.buffer[index].virt.extP[plane];
+        dstAddr = dstBuf.virt.extP[plane];        
+        if(srcAddr != NULL && dstAddr != NULL) {
+            if (mMsgEnabled & CAMERA_MSG_PREVIEW_FRAME) {
+                memcpy(dstAddr, srcAddr, dstBuf.size.extS[plane]);
+                mDataCb(CAMERA_MSG_PREVIEW_FRAME, mCamHeap, index, mCallback);
+            }           
+            
+            /* preview in HAL */
+            nativeFlushPreview(dstBuf.virt.extP[plane]);
+        }        
+    }
+
+    nativeReleaseFrame(index);
+
     return true;
 }
